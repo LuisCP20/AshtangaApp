83c9f826e984202d86a1d9806e627765068a9e9d
LuisCP20 <luis.dipa@hotmail.com>
2021-10-29 20:12:10 -0600

    color de error ahora es rojo

--- app/src/main/java/com/example/ashtanga1/ui/main/Constants.kt
+++ app/src/main/java/com/example/ashtanga1/ui/main/Constants.kt
@@ -6,5 +6,5 @@ import com.example.ashtanga1.R
 const val delayTime = 400L
 // TODO: change colors with theme colors. Dark mode
 const val rightColor = R.color.green_s_dark
-const val wrongColor = R.color.purple_p_triad
+const val wrongColor = R.color.red_p_triad
 const val defBackg = R.color.white
